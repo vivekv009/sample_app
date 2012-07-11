c64cc1518bc2050f59e4ab437c74e0615b11ecf9
genesys <genesys@gs-vbox-KiFcKt7D.(none)>
2012-07-11 04:21:13 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5797bf1cf0dca97a1f4b0de36ad84530e664d6cee52f293f849a986ae80f6dd374451e4898f12473429a33983fc85c895f1ea8ee33a03b84dd1750cf5e8e579e'
